133e4dadbbb37aecbf7ab1638e0a6abc23dab7f1
HariprakashM <hariprakashm20@gmail.com>
2022-05-03 07:16:54 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { useState } from "react";
+export default function App() {
+  const people=[
+    {
+      name:"hp",
+      pic:"https://wallpaperaccess.com/full/2213424.jpg"
+    },
+    {
+      name:"aj",
+      pic:"https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/11/alone-Best-Dp-Profile-Images-For-Instagram-photo.gif"
+    },
+    {
+      name:"rj",
+      pic:"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {people.map(a=>(
+     <Op name={a.name} pic={a.pic}/>
+     ))}
+    
+    </div>
+  );
+  
+}
+function Op({name,pic}){
+  return(
+    <div>
+      <img src={pic} alt={pic}/>
+      <h1>hi,{name}</h1>
+      <Counter/>
+    </div>
+  )
+ 
+
+
+}
+function Counter(){
+  const[like,setLike]= useState(0);
+  const[dislike,setdislike]= useState(0);
+  return (
+    <div>
+      <button onClick={()=>setLike(like+1)}>👍{like}</button>
+      <button onClick={()=>setdislike(dislike+1)}>👎{dislike}</button>
     </div>
   );
 }
-
-export default App;
